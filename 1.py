e0c271ac44c72d0f867cf4d8cced308796c02b04
Satya Srikanth <lolla_svss@me.iitr.ac.in>
2026-08-03 23:46:57 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+seeing the code what if we need to change this data recording or writing the readings event based instead of time based 
+like this is the issue what is the solution for this donot compromise on the solution we need the best one to implement 
+what is the best fix first
+
+
+
+what are all  code changes we need to do for this 
+now to implement this what codes we need to change to get us know what to do in backend 
+we will give backend code one after the other so we need to know which code to tweak exactly if this is ok we will begin 
+
